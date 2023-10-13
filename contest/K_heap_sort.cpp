ca0c61d44f114cc2fb0211379f207133afe3c7b6
matthew-mar <mmary.prog@gmail.com>
2023-10-13 11:11:58 +0300

    решил задачу на пирамидальную сортировку

--- contest/K_heap_sort.cpp
+++ contest/K_heap_sort.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <fstream>
-#include <iostream>
 
 class Heap {
 
@@ -20,7 +19,7 @@ private:
     }
 
     void shiftUp(int current) {
-        while (current > 0 && values[current] > values[parentIndex(current)]) {
+        while (current > 0 && values[current] < values[parentIndex(current)]) {
             std::swap(values[current], values[parentIndex(current)]);
             current = parentIndex(current);
         }
@@ -30,12 +29,12 @@ private:
         while (firstChildIndex(current) < values.size()) {
             int sub = current;
 
-            if (values[firstChildIndex(current)] > values[current]) {
+            if (values[firstChildIndex(current)] < values[current]) {
                 sub = firstChildIndex(current);
             }
 
             if (secondChildIndex(current) < values.size() && 
-                values[secondChildIndex(current)] > values[sub]
+                values[secondChildIndex(current)] < values[sub]
             ) {
                 sub = secondChildIndex(current);
             }
@@ -74,32 +73,46 @@ public:
     }
 };
 
-int main() {
+std::vector<int> readNumbers() {
     std::ifstream inputFile("input.txt");
-    std::ofstream outputFile("output.txt");
-    Heap heap = Heap();
 
-    while (! inputFile.eof()) {
-        std::string command;
-        inputFile >> command;
+    int count;
+    inputFile >> count;
 
-        if (command == "ADD") {
-            int value;
-            inputFile >> value;
-            heap.add(value);
-        } else if (command == "EXTRACT") {
-            if (heap.isEmpty()) {
-                outputFile << "CANNOT" << std::endl;
-            } else {
-                outputFile << heap.extract() << std::endl;
-            }
-        } else if (command == "CLEAR") {
-            heap.clear();
-        }
+    std::vector<int> numbers(count);
+    for (int i = 0; i < count; i++) {
+        inputFile >> numbers[i];
     }
 
     inputFile.close();
-    outputFile.close();
 
+    return numbers;
+}
+
+void heapsort(std::vector<int>& numbers) {
+    Heap heap = Heap();
+    for (int n : numbers) {
+        heap.add(n);
+    }
+
+    for (int i = 0; i < numbers.size(); i++) {
+        numbers[i] = heap.extract();
+    }
+}
+
+void writeNumbers(std::vector<int>& numbers) {
+    std::ofstream outputFile("output.txt");
+
+    for (int n : numbers) {
+        outputFile << n << " ";
+    }
+
+    outputFile.close();
+}
+
+int main() {
+    std::vector<int> numbers = readNumbers();
+    heapsort(numbers);
+    writeNumbers(numbers);
     return 0;
 }
